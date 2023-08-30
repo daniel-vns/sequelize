4e74741f321ec99d74dc163d11f429a2dd62e625
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-08-30 23:18:06 +0000

    sequelize

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        username: 'root',
+        password: 'root',
+        database: 'produto',
+        host: 'localhost',
+        dialect: 'mysql',
+    }
+};
---
+++ migrations/20230830230805-create_produto.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('produtos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      preco: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });   
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('produtos');
+  }
+};
